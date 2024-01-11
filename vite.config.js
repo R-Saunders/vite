f09d58c5965601b406b595a1c03a4f2d1e5fd313
Rich Saunders <rich.saunders@protonmail.com>
2024-01-11 11:00:09 +0000

    test

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "dist",
-	server: {
-		port: 3000,
-	},
+	base: "/",
 });
